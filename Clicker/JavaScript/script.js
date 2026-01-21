6103fbcceb78ea7960a35d8b295fa1582cccd5d1
sacha amaral <amaralsacha48@gmail.com>
2026-01-21 11:31:00 +0100

    maj clicker 0.5

--- Clicker/JavaScript/script.js
+++ Clicker/JavaScript/script.js
@@ -14,7 +14,7 @@ const element = {
   trumpImage: document.querySelector(".trump-image"),
   tpcText: document.querySelector("#tpc-text"),
   tpsText: document.querySelector("#tps-text"),
-  upgradesContainer: document.querySelector("#upgrade-container"),
+  upgradesContainer: document.querySelector("#upgrades-container"),
   saveBtn: document.querySelector("#save-btn"),
   loadBtn: document.querySelector("#load-btn"),
   upgradeTemplate: document.querySelector("#upgrade-template"),
@@ -22,14 +22,15 @@ const element = {
 
 // MAJ UI
 
-if (element.score) element.score.textContent = Math.floor(game.score)
-if (element.tpcText) element.tpcText.textContent = game.tpc
-if (element.tpsText) element.tpsText.textContent = game.tps
-
+function updateUI(){
+    if (element.score) element.score.textContent = Math.floor(game.score)
+    if (element.tpcText) element.tpcText.textContent = game.tpc
+    if (element.tpsText) element.tpsText.textContent = game.tps
+}
 // Click principal
 
 function onTrumpClick(){
-    game.score += game.tpc
+    game.score += game.tpc;
     updateUI()
 }
 
@@ -50,3 +51,40 @@ function saveGame(){
     localStorage.setItem("trump_clicker_save", JSON.stringify(game))
 }
 
+function loadGame(){
+    const raw = localStorage.getItem("trump_clicker_save")
+    if (!raw) return
+
+    try{
+        const data = JSON.parse(raw)
+
+        game.score = Number(data.score) || 0
+        game.tpc = Number(data.tpc) || 1
+        game.tps = Number(data.tps) || 0
+        game.upgrades = data.upgrades || {}
+
+        updateUI()
+    } catch (e) {
+        console.error("Sauvegarde corrompue :", e)
+    }
+}
+
+//Evènement
+
+if (element.trumpImage){
+    element.trumpImage.addEventListener("click", onTrumpClick)
+}
+
+if (element.saveBtn){
+    element.saveBtn.addEventListener("click", saveGame)
+}
+
+if (element.loadBtn){
+    element.loadBtn.addEventListener("click", loadGame)
+}
+
+//Démarrage
+
+loadGame()
+updateUI()
+startAutoIncome()
